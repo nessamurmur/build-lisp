3fdfc388972efbd29f3069ef7418193386171851
Nate West <natescott.west@gmail.com>
2014-08-12 21:15:23 -0500

    Silly eval

--- src/repl.c
+++ src/repl.c
@@ -3,6 +3,28 @@
 #include "mpc.h"
 #include <editline/readline.h>
 
+long eval_op(long x, char* operator, long y) {
+  if (strcmp(operator, "+") == 0) { return x + y; }
+  if (strcmp(operator, "-") == 0) { return x - y; }
+  if (strcmp(operator, "*") == 0) { return x * y; }
+  if (strcmp(operator, "/") == 0) { return x / y; }
+  if (strcmp(operator, "%") == 0) { return x % y; }
+  return 0;
+}
+
+long eval(mpc_ast_t* t) {
+  if (strstr(t->tag, "number")) { return atoi(t->contents); }
+  char* operator = t->children[1]->contents;
+  long child = eval(t->children[2]);
+
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    child = eval_op(child, operator, eval(t->children[i]));
+    i++;
+  }
+  return child;
+}
+
 int main(int argc, char** argv) {
   mpc_parser_t* Number     = mpc_new("number");
   mpc_parser_t* Operator   = mpc_new("operator");
@@ -12,7 +34,7 @@ int main(int argc, char** argv) {
   mpca_lang(MPCA_LANG_DEFAULT,
     "                                                             \
       number     : /-?[0-9]+/ ;                                   \
-      operator   : '+' | '-' | '*' | '/' ;                        \
+      operator   : '+' | '-' | '*' | '/' | '%' ;                  \
       expression : <number> | '(' <operator> <expression>+ ')' ;  \
       lispy      : /^/ <operator> <expression>+ /$/ ;             \
     ",
@@ -26,7 +48,8 @@ int main(int argc, char** argv) {
     add_history(input);
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lispy, &r)) {
-      mpc_ast_print(r.output);
+      long result = eval(r.output);
+      printf("%li\n", result);
       mpc_ast_delete(r.output);
     } else {
       mpc_err_print(r.error);
